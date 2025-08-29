5aec05fa3c66f6282ad9828f9dc4623d7d2618dd
Kevin Lopes <kevinlopes2304@gmail.com>
2025-08-29 00:17:57 -0300

    feat: add currency formatting and expense submission
    - Format amount input as BRL currency on input
    - Handle form submission to create expense object
    - Update placeholder to include currency symbol

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 // Seleciona os elementos do formulário.
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
+//Captura o evento de input para formatar o valor.
 amount.oninput = () => {
+  // Obtém o valor atual do input e remove os caracteres não numéricos.
   let value = amount.value.replace(/\D/g, "");
 
-  amount.value = value;
+  //Transforma o valor em centavos
+  value = Number(value) / 100;
+
+  //Atualiza o valor do input
+  amount.value = formatCurrencyBRL(value);
+};
+
+function formatCurrencyBRL(value) {
+  //Formata o valor no padrão BRL (Real Brasileiro)
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  //Retorna o valor formatado.dd
+  return value;
+}
+
+//Captura o evento de submit do formulário.
+form.onsubmit = (event) => {
+  //Previne o comportamento de recarregar a página.
+  event.preventDefault();
+
+  //Cria um objeto com os detalhes da nova despesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    createdAt: new Date(),
+  };
 };
